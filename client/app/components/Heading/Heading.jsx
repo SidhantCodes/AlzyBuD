55d5806b603aaa244451dc2540d3e02e3d8b481d
Sidhant Mishra <sidhant.mishra2022@vitstudent.ac.in>
2025-02-03 13:25:32 +0530

    fixed image import route

---
+++ client/app/components/Heading/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heading = ({heading}) => {
+  return (
+    <h1 className='text-[3rem] my-10 font-bold text-[#63CE68] text-center'>
+      {heading}
+    </h1>
+  )
+}
+
+export default Heading
--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,11 +1,17 @@
 
+import Heading from "./components/Heading/Heading";
 import Navbar from "./components/Navbar/Navbar";
+import SubHeading from "./components/SubHeading/SubHeading";
 
 export default function MainPage() {
   return (
     <>
-      <div className="flex py-10">
+      <div className="flex-col py-10">
         <Navbar />
+        <div className="title mt-28">
+          <SubHeading subhead={`Your gateway to streamlined `} />
+          <Heading heading={`Alzheimer's care solutions`} />
+        </div>
       </div>
     </>
   );
